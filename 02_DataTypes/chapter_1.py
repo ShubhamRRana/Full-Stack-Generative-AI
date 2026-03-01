0bdaa9b3a4be5c7b3fae52173391befd061f4b62
ShubhamRRana <shubham.r0098@gmail.com>
2026-03-01 16:32:17 +0530

    Complete section 3

---
+++ 02_DataTypes/chapter_1.py
@@ -0,0 +1,9 @@
+sugar_amount = 2
+print(f"Sugar amount : {sugar_amount}")
+sugar_amount = 12
+print(f"Sugar amount : {sugar_amount}")
+
+print(f"Id of 2: {id(2)}")
+print(f"Id of 2: {id(12)}")
+
+print(f"Sugar amount : {sugar_amount}")
---
+++ 02_DataTypes/chapter_2.py
@@ -0,0 +1,11 @@
+spice_mix = set()
+print(f"Initial spice mix id : {id(spice_mix)}")
+print(f"Initial spice mix : {spice_mix}")
+spice_mix.add("Ginger")
+spice_mix.add("Lemo")
+spice_mix.add("Cardamom")
+print(f"Initial spice mix : {spice_mix}")
+print(f"After spice mix id : {id(spice_mix)}")
+
+total_tea_leaves = 1_000_000 #underscore here can be used as comma for better readability.
+print(f"Total tea leaves : {total_tea_leaves}")
